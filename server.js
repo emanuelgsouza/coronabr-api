05c6c080706632f860f610ac26c3f09b6e808df9
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-03-18 00:05:55 -0300

    api: implement API with fastify

---
+++ server.js
@@ -0,0 +1,77 @@
+require('dotenv').config()
+const pkg = require('./package.json')
+const brData = require('./data/data-br.json')
+
+const getDataFromRange = (values, to, from) => {
+  const toTimestamp = new Date(to).getTime()
+  const fromTimestamp = new Date(from).getTime()
+
+  return values.reduce((acc, item) => {
+    const currentTimestamp = new Date(item.date).getTime()
+
+    if (currentTimestamp >= fromTimestamp && currentTimestamp <= toTimestamp) {
+      acc.push(item)
+    }
+
+    return acc
+  }, [])
+}
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async () => {
+  return {
+    version: pkg.version,
+    up: true
+  }
+})
+
+fastify.get('/states', async (request) => {
+  const { query = {} } = request
+  const { to, from } = query
+
+  if (!to || !from) {
+    return {
+      error: false,
+      timestamp: Date.now(),
+      data: {
+        values: brData.data,
+        lastUpdated: brData.lastUpdated
+      }
+    }
+  }
+
+  return {
+    error: false,
+    timestamp: Date.now(),
+    data: {
+      values: getDataFromRange(brData.data, to, from),
+      lastUpdated: brData.lastUpdated
+    }
+  }
+})
+
+fastify.get('/today', async () => {
+  return {
+    error: false,
+    timestamp: Date.now(),
+    data: {
+      values: brData.data[brData.data.length - 1],
+      lastUpdated: brData.lastUpdated
+    }
+  }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(process.env.PORT)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
