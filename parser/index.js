a84f37fc722753a5e987a08bb45eb0b70f15e93e
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-03-19 18:34:23 -0300

    refactor: split brazilian data in states and save JSONs

--- parser/index.js
+++ parser/index.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
+const { omit } = require('lodash')
 
-const lastUpdated = Date.now()
 const STATES_UIDS = require('../data/states-uids.json')
 
-const getData = async () => {
+const loadExternalData = async () => {
+  const lastUpdated = Date.now()
   try {
     const URL = `${process.env.DATABASES_URL}?v=${lastUpdated}`
     const response = await axios.get(URL)
@@ -21,30 +22,101 @@ const getData = async () => {
 
 const getPath = filePath => path.join(__dirname, `../data/${filePath}`)
 
-const getDataToSave = (locationType, data, lastUpdated) => {
+const factoryDataToSave = (location, data, lastUpdated) => {
   return JSON.stringify({
-    locationType,
+    location,
     data,
     lastUpdated
   })
 }
 
-const processData = data => {
-  return data.map(item => {
-    const { date, time } = item
+const saveDataToJSON = (statesData, brazilianData) => {
+  console.log('Saving brazilian data')
+
+  try {
+    const lastUpdated = Date.now()
+    fs.writeFileSync(
+      getPath('brazil.json'),
+      factoryDataToSave('brazil', brazilianData, lastUpdated)
+    )
+
+    Object.values(statesData).forEach(state => {
+      const name = `states/${state.state}.json`
+      fs.writeFileSync(
+        getPath(name), factoryDataToSave(state.state, state, lastUpdated)
+      )
+    })
+
+    return Promise.resolve(true)
+  } catch (e) {
+    console.error('Houve um erro no armazenamento dos dados')
+    return Promise.reject(new Error(e.message))
+  }
+}
+
+const toPlainData = (rawData = []) => {
+  return rawData.reduce((acc, data) => {
+    const { values, date, time } = data
     const [day, month, year] = date.split('/')
 
-    return {
-      date: `${year}-${month}-${day}`,
-      time,
-      fullDate: `${year}-${month}-${day} ${time}:00`,
-      values: item.values.map(stateData => {
+    return [
+      ...acc,
+      ...values.map(stateData => {
         return {
           ...stateData,
+          date: `${year}-${month}-${day}`,
+          time,
+          fullDate: `${year}-${month}-${day} ${time}:00`,
           state: STATES_UIDS[stateData.uid]
         }
-      }),
-      accumulated: item.values.reduce((acc, data) => {
+      })
+    ]
+  }, [])
+}
+
+const processDataToStates = (plainData = []) => {
+  return plainData.reduce((acc, item) => {
+    const data = omit(item, ['state'])
+
+    if (acc[item.state]) {
+      acc[item.state].dates.push(data)
+    } else {
+      acc[item.state] = {
+        state: item.state,
+        dates: [data]
+      }
+    }
+
+    return acc
+  }, {})
+}
+
+const processDataToBrasil = (plainData = []) => {
+  const datesData = plainData.reduce((acc, item) => {
+    if (acc[item.date]) {
+      acc[item.date].values.push(item)
+    } else {
+      const { date, time, fullDate } = item
+
+      acc[item.date] = {
+        date,
+        time,
+        fullDate,
+        values: [item]
+      }
+    }
+
+    return acc
+  }, {})
+
+  return Object.values(datesData).map(item => {
+    const { date, time, fullDate, values } = item
+
+    return {
+      date,
+      time,
+      fullDate,
+      accumulated: values.reduce((acc, data) => {
         acc['suspects'] += data.suspects || 0
         acc['refuses'] += data.refuses || 0
         acc['cases'] += data.cases || 0
@@ -63,36 +135,15 @@ const processData = data => {
   })
 }
 
-const saveData = (data) => {
-  const FILES = {
-    BR: 'data-br.json',
-    WORLD: 'data-world.json'
-  }
-
-  console.log('Saving brazilian data')
-
-  try {
-    const lastUpdated = Date.now()
-    fs.writeFileSync(
-      getPath(FILES.BR), getDataToSave('brazil', processData(data.brazil), lastUpdated)
-    )
-
-    fs.writeFileSync(
-      getPath(FILES.WORLD), getDataToSave('world', data.world, lastUpdated)
-    )
-
-    return Promise.resolve(true)
-  } catch (e) {
-    console.error('Houve um erro no armazenamento dos dados')
-    return Promise.reject(new Error(e.message))
-  }
-}
-
 const parser = async () => {
   try {
     console.log('Buscando os dados...')
-    const data = await getData()
-    await saveData(data)
+    const rawData = await loadExternalData()
+    const plainData = toPlainData(rawData.brazil)
+    const statesData = processDataToStates(plainData)
+    const brazilianData = processDataToBrasil(plainData)
+    
+    await saveDataToJSON(statesData, brazilianData)
     console.log('Dados salvos...')
     process.exit(0)
   } catch (e) {
